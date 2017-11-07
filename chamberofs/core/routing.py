9a78f9ce5fcdece54088f433494db3ee4288567a
Andrew Komar <anko@unisport.dk>
2017-11-07 11:59:54 +0100

    Move to websockets

--- chamberofs/core/routing.py
+++ chamberofs/core/routing.py
@@ -1,5 +1,7 @@
 from channels.routing import route
+from core.consumers import ws_message
+
 channel_routing = [
-    route("http.request", "core.consumers.http_consumer"),
+    route("websocket.receive", ws_message),
 ]
 
