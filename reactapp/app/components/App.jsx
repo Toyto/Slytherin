a11434b0657d5094a67d51a6fd6a9b73dc25653c
Andrew <wertuk2@gmail.com>
2017-11-15 21:01:14 +0200

    Hot reload

---
+++ reactapp/app/components/App.jsx
@@ -0,0 +1,11 @@
+var React = require('react');
+
+export default class App extends React.Component {
+    render(){
+        return(
+            <div>
+                <h1>Hello, man!</h1>
+            </div>
+            )
+    }
+}
--- reactapp/app/index.jsx
+++ reactapp/app/index.jsx
@@ -1,6 +1,6 @@
-var React = require('react');
-var ReactDOM = require('react-dom');
-var App = require('./components/App');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/app';
 
 ReactDOM.render(
     <App />,
