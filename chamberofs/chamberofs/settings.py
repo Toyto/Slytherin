3ce51908ba88f4a8a2b69d2e453f23af902a95ff
Andrew <wertuk2@gmail.com>
2017-11-29 12:07:18 +0200

    Started to use Redis

--- chamberofs/chamberofs/settings.py
+++ chamberofs/chamberofs/settings.py
@@ -111,3 +111,17 @@ CHANNEL_LAYERS = {
     },
 }
 
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://redis:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
+
+
