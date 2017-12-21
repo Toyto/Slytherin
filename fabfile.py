fa73b807f14a34f5eb748bbc8fc23fd57d5fdd42
Andrew <wertuk2@gmail.com>
2017-12-21 11:55:52 +0200

    Added build system.

---
+++ fabfile.py
@@ -0,0 +1,14 @@
+from fabric.api import local
+
+
+def is_work():
+    print 'It works!'
+
+def get_version():
+    version = local("git describe --tag", capture=True)
+    return version
+
+def build():
+    local('echo %s > version.py' % (get_version()))
+    local('tar -zcvf slitherin.tar.gz ../Slitherin')
+    print 'Success!'
